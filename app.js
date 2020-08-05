02ef5bdad5523ff2db56e1c1f9522b1fe9919b2e
unknown <william.chase.carlson@gmail.com>
2020-08-05 16:18:05 -0400

    asdfsddfaads

--- app.js
+++ app.js
@@ -58,6 +58,8 @@ const User = mongoose.model('User', userSchema);
 
 app.use(session({resave: true, saveUninitialized: true, secret: 'XCR3rsasa%RDHHH', cookie: {maxAge: 1000 * 60 * 60 * 24 * 7}}));
 
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
 });
