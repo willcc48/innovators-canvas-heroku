9fb7ce53d2eb984dcb0f0ff58fabfa0257fda24c
unknown <william.chase.carlson@gmail.com>
2020-08-05 16:09:18 -0400

    asdfsddfaads

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ const User = mongoose.model('User', userSchema);
 app.use(session({resave: true, saveUninitialized: true, secret: 'XCR3rsasa%RDHHH', cookie: {maxAge: 1000 * 60 * 60 * 24 * 7}}));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/frontend/build/main.7dde50a7.chunk.js'));
+    res.sendFile(path.join(__dirname+'/frontend/build/static/js/main.7dde50a7.chunk.js'));
   });
 
 app.get('/', function(req, res, next) {
